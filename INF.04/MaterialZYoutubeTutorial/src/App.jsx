b15125551ac26cf459574f57005913b7dc0484be
Filip Grala <filip.grala.1602@gmail.com>
2026-09-17 15:22:07 +0200

    Stworzono folder z zadaniem egzaminacyjnym z youtube

---
+++ INF.04/MaterialZYoutubeTutorial/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const App = () => {
+
+
+  return (
+    <div>
+      <h1>Egzamin Zawodowy INF.04 - rozwiązuje doświadczony programista</h1>
+
+      <h2>Tutaj kod</h2>
+    </div>
+  );
+};
+
+export default App;

63d9eb5aefbf420c64d587ad134c4cab0a58c64b
Filip Grala <filip.grala.1602@gmail.com>
2026-09-18 11:49:57 +0200

    Kontynuacja robienia strony z tutorialu

--- INF.04/MaterialZYoutubeTutorial/src/App.jsx
+++ INF.04/MaterialZYoutubeTutorial/src/App.jsx
@@ -1,14 +1,36 @@
-import React, { useState } from 'react';
+//Skończyłem na minucie 1:05:15
+//import bootstrap from 'bootstrap';
+import { useState } from 'react';
 
 const App = () => {
 
 
   return (
     <div>
+      <a href="https://www.youtube.com/watch?v=COtrqVRwO7I">Link do tutorialu</a>
       <h1>Egzamin Zawodowy INF.04 - rozwiązuje doświadczony programista</h1>
+      <h2>Liczba kursów: 3</h2>
+      <ol>
+        <li>Programowanie w C#</li>
+        <li>Angular dla początkujących</li>
+        <li>Kurs Django</li>
+      </ol>
 
-      <h2>Tutaj kod</h2>
+      <form>
+        <div className="form-group">
+          <label for="name-and-surname">Imię i nazwisko:</label>
+          <input className="form-control" type="text" id="name-and-surname"></input>
+        </div>
+        <div className="form-group">
+          <label for="course-number">Numer kursu:</label>
+          <input className="form-control" type="number" id="course-number"></input>
+        </div>
+
+        <button className="btn btn-primary">Zapisz do kursu</button>
+
+      </form>
     </div>
+    
   );
 };
 

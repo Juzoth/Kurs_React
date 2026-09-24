7e62e3faf5e19b4f15c387b6915f243d1b029d99
Filip Grala <filip.grala.1602@gmail.com>
2026-09-24 15:12:28 +0200

    Skończyłem zadanie zgodnie z tutorialem

--- INF.04/MaterialZYoutubeTutorial/src/App.jsx
+++ INF.04/MaterialZYoutubeTutorial/src/App.jsx
@@ -1,33 +1,70 @@
 //Skończyłem na minucie 1:05:15
-//import bootstrap from 'bootstrap';
+
 import { useState } from 'react';
+import bootstrap from 'react';
 
 const App = () => {
 
+  const [courses, setCourses] = useState([
+    'Programowanie w C#',
+    'Angular dla początkujących',
+    'Kurs Django',
+  ]);
+
+  const [nameAndSurname, setNameAndSurname] = useState();
+  const [courseNumber, setCourseNumber] = useState();
+
+  const onNameAndSurnameChange = (event) => {
+            setNameAndSurname(event.target.value);
+            };
+
+  const onCourseNumberChange = (event) => {
+              setCourseNumber(event.target.value);
+            };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(nameAndSurname);
+    if(courses[courseNumber -1]){
+      console.log(courses[courseNumber -1]);
+    } else{
+      console.log("Nieprawidłowy numer kursu")
+    }
+  };
 
   return (
     <div>
       <a href="https://www.youtube.com/watch?v=COtrqVRwO7I">Link do tutorialu</a>
       <h1>Egzamin Zawodowy INF.04 - rozwiązuje doświadczony programista</h1>
-      <h2>Liczba kursów: 3</h2>
+      <h2>Liczba kursów: {courses.length}</h2>
       <ol>
-        <li>Programowanie w C#</li>
-        <li>Angular dla początkujących</li>
-        <li>Kurs Django</li>
+        {courses.map((course) => (
+          <li>{course}</li>
+          ))}
       </ol>
 
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label for="name-and-surname">Imię i nazwisko:</label>
-          <input className="form-control" type="text" id="name-and-surname"></input>
+          <input 
+            onChange={onNameAndSurnameChange} 
+            className="form-control" 
+            type="text"
+            id="name-and-surname"
+          />
         </div>
+
         <div className="form-group">
           <label for="course-number">Numer kursu:</label>
-          <input className="form-control" type="number" id="course-number"></input>
+          <input 
+            onChange={onCourseNumberChange}
+            className="form-control" 
+            type="number" 
+            id="course-number"
+          />
         </div>
 
         <button className="btn btn-primary">Zapisz do kursu</button>
-
       </form>
     </div>
     
